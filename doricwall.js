394c611d61944459031af6c0b7cb2e290dbf4ea7
Juan La Jara <juanlajara@gmail.com>
2013-08-06 21:30:18 -0500

    compile attempt

--- doricwall.js
+++ doricwall.js
@@ -1,33 +1,34 @@
 /*jshint laxcomma:true, smarttabs:true, forin:true, noarg:true, noempty:true, eqeqeq:true,
  laxbreak:true, bitwise:true, strict:true, undef:true, unused:true, curly:true, browser:true,
   indent:4, maxerr:50 */
-
+ 
 var doricModule = angular.module( 'me.wxo.doricwall', [] );
 
-doricModule.directive( 'doricwall', [ '$timeout', function ( $timeout ) {
+doricModule.directive( 'doricwall', function ( $rootScope, $parse, $timeout ) {
+
 	"use strict";
-	var link
-	  , elements = []
-	  , parent
+
+	var compile
+	  , link
 	  , doric
-	  , doricThis
+	  , controller
 	  ;
 
-	doricThis = function ( doricVar ) {
-		doric( { elements : elements
-		       , colCount : +doricVar.replace(/col-/,'')
-		       } );
-	};
+	// controller = function ( $scope ) {
+	// 	console.log( $scope );
+	// }
 
 	doric = function ( givens ) {
 		var elements  = givens.elements || elements
 		  , columns   = []
 		  , colCount  = givens.colCount
 		  , cc        = givens.colCount
+		  , parent    = givens.parent
 		  , i         = 0
 		  , container = $( document.createDocumentFragment() )
 		  ;
-
+		// console.log( $(parent) );
+		// parent = $(parent);
 		parent.empty();
 
 		while ( colCount-- ) {
@@ -46,29 +47,36 @@ doricModule.directive( 'doricwall', [ '$timeout', function ( $timeout ) {
 		container.prependTo( parent );
 	};
 
-	link = function ( scope, element ) {
-		elements.push( element );
+	compile = function ( tElement, tAttrs, transclude ) {
+		var rpt    = document.createAttribute('ng-repeat')
+		  , holder = $(tElement)
+		  , scope  = $rootScope.$new()
+		  , items
+		  ;
 
-		if (scope.$last === true) {
+		rpt.nodeValue = tAttrs.element;
+		tElement[0].children[0].attributes.setNamedItem(rpt);
 
-			$timeout(function () {
-				parent = elements[0].parent().hasClass('doricwall') ? elements[0].parent() : elements[0].parent().parent() ;
-				responsivar( { stylesheetName : 'responsivar'
-				             , mediaQueries   : [ '@media screen and (min-width: 980px)'
-				                                , '@media screen and (min-width: 768px) and (max-width: 979px)'
-				                                , '@media screen and (min-width: 481px) and (max-width: 767px)'
-				                                , '@media screen and (max-width: 480px)'
-				                                ]
-				             , change         : doricThis
-				             } );
+		transclude( scope, function ( clone ) {
+			console.log( clone[0].innerHTML );
+		} )
 
-			});
+		return function ( scope, element, attr ) {
+			var rhs     = attr.element.split(' in ')[1];
 
+			items = scope.items = $parse(rhs)(scope);
+			console.log(scope.items);
 		}
-	};
 
-	return { restrict   : 'A'
-	       , link       : link
-	       };
+	}
 
-} ] );
+	return { restrict   : 'E'
+	       // , link       : link
+	       // , controller : controller
+	       , transclude : 'element'
+	       , replace    : true
+	       , scope      : true
+	       , template   : '<div class="doricwall"><article class="doric-brick" ng-transclude></article></div>'
+	       , compile    : compile
+	       }
+} );
